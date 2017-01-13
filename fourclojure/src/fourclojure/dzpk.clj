26e09a61a317164944bbbd2aada7b4f34f51582f
admin <liuzhen@basecity.com>
2017-01-14 07:32:59 +0800

    完成牌型筛选逻辑，代码有待重构

--- fourclojure/src/fourclojure/dzpk.clj
+++ fourclojure/src/fourclojure/dzpk.clj
@@ -1,10 +1,9 @@
 (ns fourclojure.dzpk)
 
 (defn convert-pais
-  "将牌面数值有keyword形式转换为数值形式"
-  [pais]
-  (let [k-v {:2 2 :3 3 :4 4 :5 5 :6 6 :7 7 :8 8 :9 9 :t 10 :j 11 :q 12 :k 13 :a 14}]
-    (mapv #(assoc % 1 (k-v (second %))) pais)))
+  "牌型显示形式与程序内部表现形式转换"
+  [k-v pais]
+  (mapv #(assoc % 1 (k-v (second %))) pais))
 
 (defn sort-by-num
   "根据牌面数值大小排序"
@@ -14,117 +13,185 @@
 ;;===============================
 ;; 牌型判断
 
-(defn royal-flush?
-  "判断一手牌是否为皇家同花顺"
-  [pais]
-  (and (= (sort (map last pais))
-          [10 11 12 13 14])
-       (apply = (map first pais))))
-
 (defn straight-flush?
   "判断一手牌是否为同花顺"
   [pais]
   (let [count-pais (count pais)
         sorted-nums (sort (map last pais))
-        target-nums (range (first sorted-nums) (+ 2 count-pais))]
+        target-nums (range (first sorted-nums) (+ count-pais (first sorted-nums)))]
     (and (> count-pais 4)
          (apply = (map first pais))
          (= sorted-nums target-nums))))
 
-(defn four-of-a-kind?
-  "判断一手牌是否为四炸"
+;; ============================
+(defn guess-flush
+  "获取可能组成同花类的牌型"
+  [group-by-color]
+  (->> (filter #(> (count (last %)) 4) group-by-color)
+       first
+       last
+       (#(sort-by-num % >))))
+
+(defn royal-flush-filter
+  "过滤出皇家同花顺"
   [pais]
-  (let [sorted-nums (sort (map last pais))]
-    (or (apply = (rest sorted-nums))
-        (apply = (butlast sorted-nums)))))
+  (let [group-by-color (group-by first pais)
+        guess-flush (guess-flush group-by-color)
+        royal-flush? (when-not (empty? guess-flush)
+                       (= (sort (map last guess-flush))
+                          [10 11 12 13 14]))]
+    (when royal-flush?
+      [:royal-flush guess-flush])))
 
-(defn full-house?
-  "判断一手牌是否为葫芦"
+(defn longest-straight
+  "取得最长的'顺子'"
+  [guess-straight]
+  (reduce (fn [r val]
+            (cond
+              (= 5 (count r)) r
+              (= (last (last r)) (inc (last val))) (conj r val)
+              (not= (last (last r)) (inc (last val))) [val]))
+          [] guess-straight))
+
+(defn straight-flush-filter
+  "过滤出最大的同花顺"
   [pais]
-  (let [full-house-nums? (fn [s-nums partition-n]
-                           (let [after-partition (split-at partition-n s-nums)]
-                             (and (apply = (first after-partition))
-                                  (apply = (last after-partition)))))
-        sorted-nums (sort (map last pais))]
-    (and (= 5 (count pais))
-         (or (full-house-nums? sorted-nums 2)
-             (full-house-nums? sorted-nums 3)))))
+  (let [group-by-color (group-by first pais)
+        guess-straight-flush (longest-straight (guess-flush group-by-color))]
+    (when (and (not-empty guess-straight-flush) (straight-flush? guess-straight-flush))
+      [:straight-flush guess-straight-flush])))
 
-(defn flush?
-  "判断一手牌是否为同花"
+(defn four-of-a-kind-filter
+  "过滤出最大的四带一"
   [pais]
-  (and (> (count pais) 4)
-       (apply = (map first pais))))
+  (let [group-by-num (group-by last pais)
+        guess-4 (->> (filter #(= (count (last %)) 4) group-by-num)
+                     first)
+        guess-4-1 (when guess-4
+                    (->> (filter #(not= (first guess-4) (last %)) pais)
+                         (#(sort-by-num % >))
+                         first))]
+    (when-not (empty? guess-4-1)
+      [:4-of-a-kind (conj (last guess-4) guess-4-1)])))
 
-(defn straight?
-  "判断一手牌是否为顺子"
+(defn full-house-filter
+  "过滤出最大的葫芦"
   [pais]
-  (let [count-pais (count pais)
-        sorted-nums (sort (map last pais))
-        target-nums (range (first sorted-nums) (+ 2 count-pais))]
-    (and (> count-pais 4)
-         (= sorted-nums target-nums))))
+  (let [group-by-num (group-by last pais)
+        guess-three (->> (filter #(= (count (last %)) 3) group-by-num)
+                         (sort #(> (first %1) (first %2)))
+                         first)
+        guess-two (when-not (empty? guess-three)
+                    (->> (dissoc group-by-num (first guess-three))
+                         (filter #(>= (count (last %)) 2))
+                         (sort #(> (first %1) (first %2)))
+                         first
+                         (take 2)))]
+    (when-not (empty? guess-two)
+      [:full-house (into (last guess-three) (last guess-two))])))
 
-(defn three-of-a-kind?
-  "判断一手牌是否为三同"
+(defn flush-filter
+  "过滤出最大的同花"
   [pais]
-  (->> (map last pais)
-       (group-by identity)
-       (some #(= 3 (count (last %))))
-       (not= nil)))
+  (let [group-by-color (group-by first pais)
+        guess-flush (guess-flush group-by-color)]
+    (when-not (empty? guess-flush)
+      [:flush (vec (take 5 guess-flush))])))
 
-(defn two-pairs?
-  "判断一手牌是否为2对"
+(defn straight-filter
+  "过滤出最大的顺子"
   [pais]
-  (->> (map last pais)
-       (group-by identity)
-       (filter #(= 2 (count (last %))))
-       (#(= (count %) 2))))
+  (let [group-by-num (group-by last pais)
+        guess-straight (->> (mapv #(first (last %)) group-by-num)
+                            (#(sort-by-num % >))
+                            (longest-straight))]
+    (when (>= (count guess-straight) 5)
+      [:straight (vec (take 5 guess-straight))])))
 
-(defn one-pair?
-  "判断一手牌是否为对子"
+(defn three-of-a-kind-filter
+  "过滤出最大的三同"
   [pais]
-  (->> (map last pais)
-       distinct
-       count
-       (= (dec (count pais)))))
+  (let [group-by-num (group-by last pais)
+        guess-three (->> (filter #(= (count (last %)) 3) group-by-num)
+                         (sort #(> (first %1) (first %2)))
+                         first)
+        guess-two (when guess-three
+                    (->> (filter #(not= (first guess-three) (last %)) pais)
+                         (#(sort-by-num % >))
+                         (take 2)))]
+    (when-not (empty? guess-two)
+      [:three-of-a-kind (into (last guess-three) guess-two)])))
 
-(defn high-card?
-  "判断一手牌是否是高张"
+(defn two-pairs-filter
+  "过滤出最大的两对"
   [pais]
-  (let [nums (map last pais)]
-    (and (not (straight? pais))
-         (= nums (distinct nums)))))
+  (let [group-by-num (group-by last pais)
+        guess-two (->> (filter #(= (count (last %)) 2) group-by-num)
+                       (sort #(> (first %1) (first %2)))
+                       (take 2)
+                       (map last)
+                       (#(when-not (empty? %)
+                           (apply into [] %))))
+        not-include-fn (fn [vs v]
+                     (every? #(not= v %) vs))
+        guess-single (when (= (count guess-two) 4)
+                      (->> (filter #(not-include-fn guess-two %) pais)
+                           (#(sort-by-num % >))
+                           first))]
+    (when-not (empty? guess-single)
+      [:two-pairs (conj guess-two guess-single)])))
 
-(defn mk-paixing-filter
-  "构造牌型过滤器"
-  [paixing-name check-fn]
-  (fn [[curr-paixing pais]]
-    (if curr-paixing
-      [curr-paixing pais]
-      (if (check-fn pais)
-        [paixing-name pais]
-        [curr-paixing pais]))))
-
-(defn paixing-filters
-  "牌型过滤器，必须依照牌型从大到小的顺序组装各个过滤器。"
+(defn one-pair-filter
+  "过滤出最大的一对"
   [pais]
-  (-> [nil pais]
-      ((mk-paixing-filter :royal-flush royal-flush?))
-      ((mk-paixing-filter :straight-flush straight-flush?))
-      ((mk-paixing-filter :4-of-a-kind four-of-a-kind?))
-      ((mk-paixing-filter :full-house full-house?))
-      ((mk-paixing-filter :flush flush?))
-      ((mk-paixing-filter :straight straight?))
-      ((mk-paixing-filter :three-of-a-kind three-of-a-kind?))
-      ((mk-paixing-filter :two-pairs two-pairs?))
-      ((mk-paixing-filter :one-pair one-pair?))
-      ((mk-paixing-filter :high-card high-card?))))
+  (let [group-by-num (group-by last pais)
+        guess-pair (->> (filter #(= (count (last %)) 2) group-by-num)
+                        (sort #(> (first %1) (first %2)))
+                        first)
 
+        guess-single (when guess-pair
+                       (->> (filter #(not= (first guess-pair) (last %)) pais)
+                            (#(sort-by-num % >))
+                            (take 4)))]
+    (when guess-single
+      [:one-pair (into (last guess-pair) guess-single)])))
 
+(defn high-card-filter
+  "过滤出最大的高张"
+  [pais]
+  [:high-card (->> (sort-by-num pais >)
+                   (take 5)
+                   vec)])
 
+(defn mk-filter
+  "构建一个filter"
+  [filter-fn]
+  (fn [[type pais]]
+    (if type
+      [type pais]
+      (if-let [type-pai (filter-fn pais)]
+        type-pai
+        [nil pais]))))
+
+(defn comp-filters
+  "构建大筛子，从大到小牌型进行组装"
+  [pais]
+  (->> [nil pais]
+       ((mk-filter royal-flush-filter))
+       ((mk-filter straight-flush-filter))
+       ((mk-filter four-of-a-kind-filter))
+       ((mk-filter full-house-filter))
+       ((mk-filter flush-filter))
+       ((mk-filter straight-filter))
+       ((mk-filter three-of-a-kind-filter))
+       ((mk-filter two-pairs-filter))
+       ((mk-filter one-pair-filter))
+       ((mk-filter high-card-filter))))
 
 ;;=============================
-;(defn paixing
-;  [pais]
-;  )
+(defn paixing
+  "入口，找出德州扑克7中牌中最大的牌型"
+  [pais]
+  (let [converted-pais (convert-pais {:2 2 :3 3 :4 4 :5 5 :6 6 :7 7 :8 8 :9 9 :t 10 :j 11 :q 12 :k 13 :a 14} pais)
+        [type paixing-pais] (comp-filters converted-pais)]
+    [type (convert-pais {2 :2 3 :3 4 :4 5 :5 6 :6 7 :7 8 :8 9 :9 10 :t 11 :j 12 :q 13 :k 14 :a} paixing-pais)]))
--- fourclojure/test/fourclojure/dzpk_test.clj
+++ fourclojure/test/fourclojure/dzpk_test.clj
@@ -16,7 +16,7 @@
 
 (deftest convert-pais-test
   (are [pais result]
-    (= (convert-pais pais) result)
+    (= (convert-pais {:2 2 :3 3 :4 4 :5 5 :6 6 :7 7 :8 8 :9 9 :t 10 :j 11 :q 12 :k 13 :a 14} pais) result)
     [[:s :2] [:s :3] [:s :4] [:s :5] [:s :6] [:d :7] [:d :8] [:d :9] [:d :t] [:d :j] [:d :q] [:d :k] [:d :a]]
     [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6] [:d 7] [:d 8] [:d 9] [:d 10] [:d 11] [:d 12] [:d 13] [:d 14]]))
 
@@ -31,122 +31,23 @@
     [[:s 2] [:s 8] [:s 7] [:d 6] [:d 4]]
     [[:s 8] [:s 7] [:d 6] [:d 4] [:s 2]]))
 
-(deftest royal-flush?-test
-  (are [pais result]
-    (= (royal-flush? pais) result)
-    [[:s 10] [:s 11] [:s 12] [:s 13] [:s 14]] true
-    [[:s 10] [:s 11] [:s 12] [:s 13] [:d 14]] false
-    [[:s 9] [:s 10] [:s 11] [:s 12] [:s 13] [:s 14]] false
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6] [:s 8]] false
-    [[:s 2] [:s 3] [:s 4] [:s 5]] false))
 
-(deftest four-of-a-kind?-test
-  (are [pais result]
-    (= (four-of-a-kind? pais) result)
-    [[:c 7] [:s 7] [:h 7] [:d 7] [:d 6]] true
-    [[:c 7] [:s 7] [:h 7] [:d 7] [:d 10] [:d 11]] false
-    [[:c 7] [:s 7] [:h 7] [:d 6] [:d 11]] false))
+(deftest 测试德州扑克最优牌型
+         (are [pais result]
+              (= (paixing pais) result)
+              [[:s :2] [:s :3] [:s :4] [:s :5] [:s :6] [:d :3] [:d :4]]
+              [:straight-flush [[:s :6] [:s :5] [:s :4] [:s :3] [:s :2]]]
 
-(deftest full-house?-test
-  (are [pais result]
-    (= (full-house? pais) result)
-    [[:d 11] [:c 11] [:s 11] [:d 12] [:s 12]] true
-    [[:d 11] [:c 11] [:s 11] [:d 12] [:s 13]] false
-    [[:d 11] [:c 11] [:s 11] [:d 12]] false
-    [[:d 11] [:c 11] [:s 11] [:d 12] [:s 12] [:s 13]] false))
+              [[:c :7] [:d :9] [:s :7] [:h :7] [:d :k] [:d :7] [:d :a]]
+              [:4-of-a-kind [[:c :7] [:s :7] [:h :7] [:d :7] [:d :a]]]
 
-(deftest straight-flush?-test
-  (are [pais result]
-    (= (straight-flush? pais) result)
+              [[:d :q] [:d :j] [:c :j] [:s :j] [:s :q] [:d :8] [:d :t]]
+              [:full-house [[:d :j] [:c :j] [:s :j] [:d :q] [:s :q]]]
 
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6]] true
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:d 6]] false
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6] [:s 7]] true
-    [[:s 2] [:s 3] [:s 4] [:s 5]] false))
+              [[:c :3] [:c :9] [:d :t] [:c :j] [:c :8] [:c :q] [:s :t]]
+              [:flush  [[:c :q] [:c :j] [:c :9] [:c :8] [:c :3]]]
 
-(deftest flush?-test
-  (are [pais result]
-    (= (flush? pais) result)
+              [[:s :7] [:c :6] [:d :8] [:h :5] [:c :4] [:s :8] [:h :8]]
+              [:straight [[:d :8] [:s :7] [:c :6] [:h :5] [:c :4]]]
 
-    [[:c 12] [:c 11] [:c 9] [:c 8] [:c 3]] true
-    [[:c 12] [:c 11] [:c 9] [:c 8]] false
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6]] true
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6] [:s 10]] true))
-
-(deftest straight?-test
-  (are [pais result]
-    (= (straight? pais) result)
-
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6]] true
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6] [:d 7]] true
-    [[:s 2] [:s 3] [:s 4] [:s 5]] false
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 7]] false))
-
-(deftest three-of-a-kind?-test
-  (are [pais result]
-    (= (three-of-a-kind? pais) result)
-    [[:s 2] [:d 2] [:c 2] [:h 2]] false
-    [[:s 2] [:d 3] [:c 2] [:h 2] [:d 4]] true
-    [[:s 2] [:d 4] [:c 5] [:h 2] [:d 4]] false
-    ))
-
-(deftest two-pairs?-test
-  (are [pais result]
-    (= (two-pairs? pais) result)
-
-    [[:s 2] [:d 2] [:c 2] [:h 2] [:d 3]] false
-    [[:s 2] [:d 2] [:c 4] [:h 4] [:d 3]] true
-    [[:s 2] [:d 2] [:c 2] [:h 4] [:d 3]] false
-    ))
-
-(deftest pairs?-test
-  (are [pais result]
-    (= (one-pair? pais) result)
-
-    [[:s 2] [:d 2] [:c 4] [:h 4] [:d 3]] false
-    [[:s 2] [:d 2] [:c 5] [:h 4] [:d 3]] true))
-
-(deftest single?-test
-  (are [pais result]
-    (= (high-card? pais) result)
-
-    [[:s 2] [:d 6] [:c 5] [:h 7] [:d 3]] true
-    [[:s 2] [:d 2] [:c 5] [:h 4] [:d 3]] false))
-
-(deftest mk-filter-test
-  (is (= ((mk-paixing-filter :royal-flush royal-flush?)
-           [nil [[:s 10] [:s 11] [:s 12] [:s 13] [:s 14]]])
-         [:royal-flush [[:s 10] [:s 11] [:s 12] [:s 13] [:s 14]]])))
-
-(deftest paixing-filters-test
-  (are [pais result]
-    (= (paixing-filters pais) result)
-
-    [[:s 10] [:s 11] [:s 12] [:s 13] [:s 14]] [:royal-flush [[:s 10] [:s 11] [:s 12] [:s 13] [:s 14]]]
-    [[:c 7] [:s 7] [:h 7] [:d 7] [:d 13]] [:4-of-a-kind [[:c 7] [:s 7] [:h 7] [:d 7] [:d 13]]]
-    [[:d 11] [:c 11] [:s 11] [:d 12] [:s 12]] [:full-house [[:d 11] [:c 11] [:s 11] [:d 12] [:s 12]]]
-    [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6]] [:straight-flush [[:s 2] [:s 3] [:s 4] [:s 5] [:s 6]]]
-    [[:c 12] [:c 11] [:c 9] [:c 8] [:c 3]] [:flush [[:c 12] [:c 11] [:c 9] [:c 8] [:c 3]]]
-    [[:d 2] [:s 3] [:s 4] [:s 5] [:s 6]] [:straight [[:d 2] [:s 3] [:s 4] [:s 5] [:s 6]]]
-    [[:s 2] [:d 2] [:c 4] [:h 4] [:d 3]] [:two-pairs [[:s 2] [:d 2] [:c 4] [:h 4] [:d 3]]]
-    [[:s 2] [:d 3] [:c 2] [:h 2] [:d 4]] [:three-of-a-kind [[:s 2] [:d 3] [:c 2] [:h 2] [:d 4]]]
-    [[:s 2] [:d 2] [:c 5] [:h 4] [:d 3]] [:one-pair [[:s 2] [:d 2] [:c 5] [:h 4] [:d 3]]]
-    [[:s 2] [:d 7] [:c 5] [:h 4] [:d 3]] [:high-card [[:s 2] [:d 7] [:c 5] [:h 4] [:d 3]]]))
-
-;(deftest 测试德州扑克最优牌型
-;         (are [pais result]
-;              (= (paixing pais) result)
-;              [[:s :2] [:s :3] [:s :4] [:s :5] [:s :6] [:d :3] [:d :4]]
-;              [:royal-flush [[:s :2] [:s :3] [:s :4] [:s :5] [:s :6]]]
-;
-;              [[:c :7] [:d :9] [:s :7] [:h :7] [:d :k] [:d :7] [:d :a]]
-;              [:4-of-a-kind [[:c :7] [:s :7] [:h :7] [:d :7] [:d :a]]]
-;
-;              [[:d :q] [:d :j] [:c :j] [:s :j] [:s :q] [:d :8] [:d :t]]
-;              [:full-house [[:d :j] [:c :j] [:s :j] [:d :q] [:s :q]]]
-;
-;              [[:c :3] [:c :9] [:d :t] [:c :j] [:c :8] [:c :q] [:s :t]]
-;              [:flush  [[:c :q] [:c :j] [:c :9] [:c :8] [:c :3]]]
-;
-;              [[:s :7] [:c :6] [:d :8] [:h :5] [:c :4] [:s :8] [:h :8]]
-;              [:straight [[:h :8] [:s :7] [:c :6] [:h :5] [:c :4]]]))
+              ))
