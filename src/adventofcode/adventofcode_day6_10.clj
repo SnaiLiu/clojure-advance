527c0da906b4793671a98c3af37409a17495f435
liuzhen <liuzhen@basecity.com>
2017-12-25 23:28:47 +0800

    finish day8 part one

--- src/adventofcode/adventofcode_day6_10.clj
+++ src/adventofcode/adventofcode_day6_10.clj
@@ -322,3 +322,57 @@
   (u/handle-from-file "src/adventofcode/adventofcodeinputs/day7.txt"
                       (input-converse weight-for-balance))
   )
+
+;;---------------------------------------------------------
+;; Day 8
+(defn parse-instruction
+  "Parse the instruction "
+  [instruction]
+  (let [[register ops amount _ cond-arg comp-ops comp-val] (clojure.string/split instruction #" ")
+        ops-fn ({"inc" (fnil + 0) "dec" (fnil - 0)} ops)
+        comp-fn ({">"  > ">=" >= "<" < "<=" <= "==" == "!=" not=} comp-ops)]
+    {:cond-arg cond-arg
+     :register register
+     :amount   (Long/parseLong amount)
+     :handler  (fn [reg amount cond-a]
+                 (if (comp-fn cond-a (Long/parseLong comp-val))
+                   (ops-fn reg amount)
+                   reg))}))
+
+(defn largest-register
+  "--- Day 8: I Heard You Like Registers (Part One)---
+   Each instruction consists of several parts: the register to modify, whether
+   to increase or decrease that register's value, the amount by which to increase
+   or decrease it, and a condition. If the condition fails, skip the instruction
+   without modifying the register. The registers all start at 0. The instructions
+   look like this:
+   - b inc 5 if a > 1
+   - a inc 1 if b < 5
+   - c dec -10 if a >= 1
+   - c inc -20 if c == 10
+   These instructions would be processed as follows:
+   Because a starts at 0, it is not greater than 1, and so b is not modified.
+   a is increased by 1 (to 1) because b is less than 5 (it is 0).
+   c is decreased by -10 (to 10) because a is now greater than or equal to 1 (it is 1).
+   c is increased by -20 (to -10) because c is equal to 10.
+   After this process, the largest value in any register is 1.
+   You might also encounter <= (less than or equal to) or != (not equal to). However,
+   the CPU doesn't have the bandwidth to tell you what all the registers are named, and
+   leaves that to you to determine.
+   What is the largest value in any register after completing the instructions
+   in your puzzle input?
+   ------------------------------------------------"
+  [instructions]
+  (let [registers (reduce (fn [all-registers instruction]
+                            (let [{:keys [register amount cond-arg handler]} (parse-instruction instruction)
+                                  reg-val (get all-registers register 0)]
+                              (assoc all-registers register (handler reg-val
+                                                                     (or amount 0)
+                                                                     (get all-registers cond-arg 0)))))
+                          {} instructions)
+        sorted-registers (sort-by last >= (into [] registers))]
+    (last (first sorted-registers))))
+
+(comment
+  (u/handle-from-file "src/adventofcode/adventofcodeinputs/day8_example.txt" largest-register)
+  (u/handle-from-file "src/adventofcode/adventofcodeinputs/day8.txt" largest-register))
